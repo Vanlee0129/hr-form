db505ab7c2fec4cd64ea606732c9a5e824163e8e
Vanlee0129 <33856668+Vanlee0129@users.noreply.github.com>
2021-07-02 23:09:29 +0800

    fix: 格式化代码

--- src/common/index.js
+++ src/common/index.js
@@ -1,27 +1,27 @@
 export const PHONE_ERROR = {
 	title: "手机号格式错误",
 	type: "error",
-};
+}
 
 export const CARD_ERROR = {
 	title: "身份证号格式错误",
 	type: "error",
-};
+}
 
 export const INPUT_ERROR = {
 	title: "请填写完整",
 	type: "error",
-};
+}
 
 export const REQUEST_ERROR = {
 	title: "请求失败",
 	type: "error",
-};
+}
 
 export const SYSTEM_ERROR = {
 	title: "系统错误",
 	type: "error",
-};
+}
 
 export const SIGN_INFO = {
 	name: "",
@@ -41,7 +41,7 @@ export const SIGN_INFO = {
 	endTime: "",
 	shop: "",
 	salary: "",
-};
+}
 
 export const GENDER_LIST = [
 	{
@@ -52,4 +52,4 @@ export const GENDER_LIST = [
 		value: "gender",
 		label: "女",
 	},
-];
+]
